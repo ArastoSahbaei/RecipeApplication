0f1fbb7f67140450c80e51b9ed68af684b91d417
arasto <Arasto.Sahbaei@gmail.com>
2020-07-15 13:44:13 +0200

    refactored server.js

--- server/server.js
+++ server/server.js
@@ -1,22 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const middlewares = require('./middleware');
+const dbConfig = require('./config/database.config.js');
 
-// create express app
 const app = express();
-
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
-
-// parse requests of content-type - application/json
 app.use(bodyParser.json())
 
-// Configuring the database
-const dbConfig = require('./config/database.config.js');
-const mongoose = require('mongoose');
-const middlewares = require('./middlewares');
-mongoose.Promise = global.Promise;
-
-// Connecting to the database
 mongoose.connect(dbConfig.url, {
     useNewUrlParser: true
 }).then(() => {
@@ -26,18 +17,14 @@ mongoose.connect(dbConfig.url, {
     process.exit();
 });
 
-// define a simple route
 app.get('/', (req, res) => {
     res.json({ "message": "brfSverige Application" });
 });
 
-// Require Notes routes
 require('./app/routes/note.routes.js')(app);
-
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
-// listen for requests
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
 });
