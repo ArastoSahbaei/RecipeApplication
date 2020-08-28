cc5377455cd8725813f6f111e1cdc16c66d0baf8
arasto <Arasto.Sahbaei@gmail.com>
2020-08-28 09:49:32 +0200

    added new services

--- client/src/shared/api/service/service.ts
+++ client/src/shared/api/service/service.ts
@@ -4,11 +4,26 @@ const getAllRecipes = () => {
     return http.get("/recipe");
 };
 
-const getRecipeById = (id: any) => {
-    return http.get(`/recipe/${id}`);
+const getRecipeById = (recipeId: any) => {
+    return http.get(`/recipe/${recipeId}`);
+};
+
+const deleteRecipeById = (recipeId: any) => {
+    return http.delete(`/recipe/${recipeId}`);
+};
+
+const createRecipe = (newRecipe: object) => {
+    return http.post(`/recipe/${newRecipe}`);
+};
+
+const updateRecipe = (updatedRecipe: object) => {
+    return http.put(`/recipe/${updatedRecipe}`);
 };
 
 export default {
     getAllRecipes,
-    getRecipeById
+    getRecipeById,
+    deleteRecipeById,
+    createRecipe,
+    updateRecipe
 };
