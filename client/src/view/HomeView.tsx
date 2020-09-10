24e43414cb4db2adafcb6c6a00cca17780b8035b
arasto <Arasto.Sahbaei@gmail.com>
2020-09-10 14:17:54 +0200

    will now authenticate upon visit

--- client/src/view/HomeView.tsx
+++ client/src/view/HomeView.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react'
+import { UserContext } from '../UserContext'
 
 export const HomeView = () => {
+    const [user, setUser] = useContext(UserContext)
+
+    const parseJWT = (token: any) => {
+        if (!token) { return; }
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace('-', '+').replace('_', '/');
+        const jwtToken = JSON.parse(window.atob(base64));
+        setUser({ auth: true, id: jwtToken.id })
+    }
+
+    useEffect(() => {
+        parseJWT(localStorage.getItem('JWT'))
+        return () => { }
+    }, [])
+
     return (
         <div>
             <h1>Home Page</h1>
         </div>
     )
 }
+
 
